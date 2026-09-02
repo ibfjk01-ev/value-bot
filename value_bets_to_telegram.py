3349a3185e27cab850d34f6d535b6a6e46a20fde
ibfjk01-ev <ibfjk01@gmail.com>
2026-09-02 10:29:10 +0200

    Update value_bets_to_telegram.py

--- value_bets_to_telegram.py
+++ value_bets_to_telegram.py
@@ -171,6 +171,9 @@ def filter_bets(bets: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
             continue
 
         if not allowed_market(market):
+            price = get_price_from_bet(bet)
+
+if price is None or price < 1.50 or price > 3.00:
             continue
 
         result.append(bet)
